65a5e49175db737d6c6f271b0c6bb300d7400ac2
RionREFF\x00 <127065725+rionreff-null@users.noreply.github.com>
2023-07-06 15:01:39 +0700

    Create SelectionSort.java

---
+++ JAVA/A1/SelectionSort.java
@@ -0,0 +1,29 @@
+class SelectionSort {
+    public static void main(String[] args) {
+        int[] data = { 10, 83, 52, 77, 29, 41, 3 };
+        int K = 0;
+        int L = data.length - 1;
+        
+        while(K != L) {
+            int min_index = K;
+
+            // Mencari index Nilai yang terkecil
+            for(int i=K; i<=L; i++) {
+                if(data[i] < data[min_index]) {
+                    min_index = i;
+                }
+            }
+            
+            // Menukar nilai
+            int temp = data[K];
+            data[K] = data[min_index];
+            data[min_index] = temp;
+            
+            K += 1;
+        }
+        // Menampilkan Data yang Sudah diUrutkan
+        for(int i=0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+    }
+}
